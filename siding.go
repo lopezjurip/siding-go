e80c915fd5ec25ae25d03540c5cac5ffd47127e3
Patricio López <lopezjuripatricio@gmail.com>
2015-07-13 14:35:34 -0300

    initial commit

---
+++ siding.go
@@ -0,0 +1,114 @@
+package siding
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"time"
+)
+
+/*
+Siding base URL
+*/
+const BASEURL = "https://intrawww.ing.puc.cl/siding/dirdes/ingcursos/cursos/index.phtml"
+
+/*
+Login url
+*/
+const LOGINBASEURL = "https://intrawww.ing.puc.cl/siding/index.phtml"
+
+/*
+ReadResponse to get HTML string from response
+*/
+func ReadResponse(resp *http.Response) (html string, err error) {
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+/*
+Siding class
+*/
+type Siding struct {
+	username, password string
+	client             *http.Client
+	// cookies            []*http.Cookie
+}
+
+/*
+Login to Siding
+*/
+func (s *Siding) Login() (resp *http.Response, err error) {
+	s.client = &http.Client{
+		Jar: newCookieJar(),
+	}
+
+	response, err := s.client.PostForm(LOGINBASEURL, s.postArguments())
+
+	if err != nil {
+		return nil, err
+	}
+	return response, err
+}
+
+/*
+Client for HTTP usage
+*/
+func (s *Siding) Client() (client *http.Client, err error) {
+	cookie := s.sessionCookie()
+
+	if cookie == nil || cookie.Expires.After(time.Now()) {
+		if _, err := s.Login(); err != nil {
+			return nil, err
+		}
+	}
+	return s.client, nil
+}
+
+/*
+Get request operation to URL
+*/
+func (s *Siding) Get(url string) (resp *http.Response, err error) {
+	client, err := s.Client()
+	if err != nil {
+		return nil, err
+	}
+	return client.Get(url)
+}
+
+/*
+Announcements for course with id
+*/
+func (s *Siding) Announcements(id uint) (resp *http.Response, err error) {
+	return s.Get(
+		fmt.Sprintf("%s?accion_curso=avisos&acc_aviso=mostrar&id_curso_ic=%d", BASEURL, id),
+	)
+}
+
+func (s Siding) sessionCookie() *http.Cookie {
+	url, _ := url.Parse(LOGINBASEURL)
+	if s.client != nil && len(s.client.Jar.Cookies(url)) > 0 {
+		return s.client.Jar.Cookies(url)[0]
+	}
+	return nil
+}
+
+func (s Siding) postArguments() url.Values {
+	// "login={0}&passwd={1}&sw=&sh=&cd="
+	return url.Values{
+		"login":  {s.username},
+		"passwd": {s.password},
+		"sw":     {""},
+		"sh":     {""},
+		"cd":     {""},
+	}
+}
+
+func newCookieJar() *cookiejar.Jar {
+	jar, _ := cookiejar.New(nil)
+	return jar
+}
---
+++ siding_test.go
@@ -0,0 +1,90 @@
+package siding
+
+import (
+	"flag"
+	"fmt"
+	"testing"
+)
+
+var (
+	username, password string
+	courseID           uint
+)
+
+func init() {
+	flag.StringVar(&username, "user", "", "Siding username")
+	flag.StringVar(&password, "pass", "", "Siding password")
+	flag.UintVar(&courseID, "course", 1000, "Course ID to test announcements")
+}
+
+func ValidSession() *Siding {
+	return &Siding{username: username, password: password}
+}
+
+func VerifySession(siding *Siding, t *testing.T) {
+	if session := siding.sessionCookie().Name; session != "PHPSESSID" {
+		t.Error("Expected session cookie to be named PHPSESSID, got", session)
+	}
+}
+
+func TestSessionArguments(t *testing.T) {
+	if username == "" || password == "" {
+		t.Error(`Must declare session credentials to begin testing.
+			Example: go test --user USERNAME -pass PASSWORD`)
+	}
+}
+
+func TestPostArguments(t *testing.T) {
+	siding := ValidSession()
+
+	args := siding.postArguments()
+
+	if a := args.Get("login"); a != username {
+		t.Error("Expected ", username, ", got ", a)
+	}
+
+	if a := args.Get("passwd"); a != password {
+		t.Error("Expected ", password, ", got ", a)
+	}
+
+	for _, key := range []string{"sw", "sh", "cd"} {
+		if a := args.Get(key); a != "" {
+			t.Error("Expected ", key, "to be empty")
+		}
+	}
+}
+
+func TestLogin(t *testing.T) {
+	siding := ValidSession()
+	siding.Login()
+
+	VerifySession(siding, t)
+}
+
+func TestClient(t *testing.T) {
+	siding := ValidSession()
+
+	_, err := siding.Client()
+	if err != nil {
+		t.Error("Expected valid logged session on client, got error", err)
+	}
+
+	VerifySession(siding, t)
+}
+
+func TestAnnouncements(t *testing.T) {
+	siding := ValidSession()
+
+	resp, err := siding.Announcements(courseID)
+
+	if err != nil {
+		t.Error("Got error", err)
+	}
+
+	html, err := ReadResponse(resp)
+	if err != nil {
+		t.Error("Got error while getting html: ", err)
+	}
+
+	fmt.Println(html)
+}
